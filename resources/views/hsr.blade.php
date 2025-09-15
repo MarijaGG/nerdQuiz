647e7b411f68a79c5b1eadc1dd04e66f884de4d1
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-09-15 10:49:17 +0300

    start

---
+++ resources/views/hsr.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Honkai: Star Rail Quiz</title>
+</head>
+<body>
+    <h1> Honkai: Star Rail Quiz </h1>
+        <h2>Question 1</h2>
+    <form>
+    
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hsr', function () {
+    return view('hsr');
+});
+
+Route::get('/gi', function () {
+    return view('gi');
+});
+
+Route::get('/dino', function () {
+    return view('dino');
+});
+
+Route::get('/fnaf', function () {
+    return view('fnaf');
+});
+
+Route::get('/jojo', function () {
+    return view('jojo');
+});
