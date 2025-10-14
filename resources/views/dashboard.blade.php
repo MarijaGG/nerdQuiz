6c55b56c42789e6de09306b62e1179fe23d27703
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-10-14 14:33:57 +0300

    login

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+
+    <h1 class="text-2xl font-bold mb-4 text-center">Welcome to NerdQuiz</h1>
+
+    <div class="space-y-2 text-center">
+        <a href="/hsr" class="hover:underline">HSR quiz</a> <br>
+        <a href="/gi" class="hover:underline">Genshin Impact quiz</a> <br>
+        <a href="/dino" class="hover:underline">Dinosaur quiz</a> <br>
+        <a href="/fnaf" class="hover:underline">FNaF quiz</a> <br>
+        <a href="/jojo" class="hover:underline">JJBA quiz</a>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Redirect guests to login, users to dashboard
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
-Route::get('/hsr', function () {
-    return view('hsr');
+// Dashboard (requires login)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Profile routes
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/gi', function () {
-    return view('gi');
-});
-
-Route::get('/dino', function () {
-    return view('dino');
-});
-
-Route::get('/fnaf', function () {
-    return view('fnaf');
-});
-
-Route::get('/jojo', function () {
-    return view('jojo');
-});
+require __DIR__.'/auth.php';
